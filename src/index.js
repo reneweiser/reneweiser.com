cb0a802998e10c2dcdad23f614567021169b5bf1
René Weiser <rene.weiser@uni-weimar.de>
2020-07-15 10:37:22 +0200

    Implemented simple random quote selector

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./css/style.css";
 import "./images/sp-rene.png";
+
+import Utils from "./js/utilities.js";
+
+const quotes = [
+  "Sharing some bits of my internet life",
+  "Monokai is the best color scheme there is. EVER.",
+  "I like mondays!",
+  "Clean code is very life, except when your deadline is tomorrow.",
+];
+
+const quoteContainer = document.querySelector("#random-quote");
+quoteContainer.innerText = quotes[Utils.getRandomInt(quotes.length)];
